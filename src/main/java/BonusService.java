5a456976d75ad531e59c69a00f2b66ecd4bdf793
Oleg Somov <doremi@fasollasi.ru>
2024-06-03 20:40:37 +0300

    Exercise 2 (Error)

--- src/main/java/BonusService.java
+++ src/main/java/BonusService.java
@@ -1,7 +1,7 @@
 public class BonusService {
     public long calcBonus(long amount, boolean isRegistered) {
-        int percent = isRegistered ? 3 : 1;
-        long bonus = amount * percent / 100;
+        int Percent = isRegistered ? 3 : 1;
+        long bonus = amount * Percent / 100;
         long limit = 500;
         if (bonus > limit) {
             bonus = limit;
